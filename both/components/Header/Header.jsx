495315a6eacde0589c507cd1a82169274749c43e
Jaime <jaivalz@yahoo.com>
2016-03-18 23:13:01 +0100

    First commit, started app, added users registration and materialize

---
+++ both/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+Header = React.createClass({
+  render() {
+
+    var navStyle = {
+      backgroundColor: "#3f51b5",
+      paddingLeft: "12px"
+    };
+
+    return (
+    <nav style={navStyle}>
+       <div className="nav-wrapper">
+         <a href="/" className="brand-logo">App name</a>
+         <ul id="nav-mobile" className="right hide-on-med-and-down">
+           <li><a href="/">Home</a></li>
+         </ul>
+       </div>
+      </nav>
+    );
+  }
+});
---
+++ both/pages/Home.jsx
@@ -0,0 +1,10 @@
+Home = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>Welcome to Meteor!</h1>
+
+      </div>
+    );
+  }
+});
