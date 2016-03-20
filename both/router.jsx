0f37732b7b6cb98f5753f28135aa7946020e48ab
Jaime <jaivalz@yahoo.com>
2016-03-20 18:23:04 +0100

    Added forgot password form

--- both/router.jsx
+++ both/router.jsx
@@ -8,6 +8,7 @@ Reaktor.init(
     <Route path="/" content={Home} layout={MainLayout} />
     <Route path="/register" content={Register} layout={MainLayout} triggersEnter={isLoggedIn} />
     <Route path="/login" content={Login} layout={MainLayout} triggersEnter={isLoggedIn} />
+    <Route path="/changePassword" content={ChangePassword} layout={MainLayout} triggersEnter={isNotLoggedIn} />
   </Router>
 );
 
@@ -16,6 +17,13 @@ function isLoggedIn(context, doRedirect) {
 	if(User.isLoggedIn()) {
 		doRedirect('/');
 	}
+}
+
+// Checks if user is logged in
+function isNotLoggedIn(context, doRedirect) {
+  if(!User.isLoggedIn()) {
+    doRedirect('/');
+  }
 }
 
 // Reaktor doensn't have a notFound component yet
